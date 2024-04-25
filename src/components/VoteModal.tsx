72f79ee86af39f5974ad012ed23faed777338d5c
Victor Collasanta <victorcollasanta@Victors-MacBook-Pro.local>
2024-04-25 16:35:06 -0300

    remove voterCard check

--- src/components/VoteModal.tsx
+++ src/components/VoteModal.tsx
@@ -92,27 +92,6 @@ function VoteModal({ open, onClose, grantIds, proposalId, address }: VoteModalPr
 
   const votingPower = snapshotProposal?.votesAvailable ?? 0;
 
-  const { data: balanceOf } = useContractRead({
-    address: '0xfb03372a30E173A8998f732dDfeA0138144B468e',
-    abi: [
-      {
-        inputs: [
-          { internalType: 'address', name: 'account', type: 'address' },
-          { internalType: 'uint256', name: 'id', type: 'uint256' },
-        ],
-        name: 'balanceOf',
-        outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ] as const,
-    functionName: 'balanceOf',
-    args: [address as Address, 1n],
-    chainId: 10,
-  });
-
-  const hasVoterCard = balanceOf && balanceOf > 0n;
-
   return (
     <Dialog open={open} onDismiss={handleDismiss} variant="blank">
       <Dialog.CloseButton onClick={handleDismiss} />
@@ -148,11 +127,6 @@ function VoteModal({ open, onClose, grantIds, proposalId, address }: VoteModalPr
           <DisplayItem label={`Selected proposal${grantIds.length > 1 ? 's' : ''}`} value={grantIds.join(', ')} />
         </DisplayItems>
 
-        {votingPower === 0 && !!hasVoterCard && (
-          <Helper type="warning">
-            You should have voting power but there seems to be an error with Snapshot. Try again later.
-          </Helper>
-        )}
       </InnerModal>
       <Dialog.Footer
         leading={
